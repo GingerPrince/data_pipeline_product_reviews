87aba2646b690c0766eccf03c18136b621366b94
auto-user <auto@example.com>
2026-04-28 10:22:21 +0100

    Publish processed data

--- src/data_pipelines.py
+++ src/data_pipelines.py
@@ -1,12 +1,18 @@
 from src.extract import extract_data
 from src.transform import transform_data
 from src.load import load_data
+import os 
 
 def main():
     df_reviews, products_df = extract_data()
     combined_df = transform_data(df_reviews, products_df)
-    load_data(repo_url = "https://github.com/GingerPrince/data_pipeline_product_reviews.git")  # publishes processed dataset
+    # Retrieve the token from an environment variable
+    github_token = os.getenv("GH_TOKEN")
+    
+    # Update the URL to include the token for authentication
+    repo_url = f"https://{github_token}@github.com/GingerPrince/data_pipeline_product_reviews.git"
+    
+    load_data(repo_url=repo_url)
     print("Pipeline completed successfully!")
-
 if __name__ == "__main__":
     main()
